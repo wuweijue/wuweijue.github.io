7656c3715c58682a7ca5cbe9bc72f2d887855dc7
weijuew <awuweijue@163.com>
2023-10-08 18:47:18 +0800

    测试 githubpages

--- public/books/js.md
+++
@@ -1,1 +0,0 @@
-<a name="hG8Tz"></a>
--- src/static/books/bookList.tsx
+++
@@ -1,9 +0,0 @@
-const bookList = [
-    {
-        // title: 'V8引擎-垃圾回收机制',
-        title: 'Garbage-collection-mechanism',
-        path: 'JavaScript-low-level-design'
-    }
-]
-
-export default bookList
